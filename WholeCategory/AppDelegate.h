4afcbbfe7a210a3afd9f854c1c38e9c1089acc3b
zb1006801184 <1006801184@qq.com>
2017-02-21 14:24:30 +0800

    Initial Commit

---
+++ WholeCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WholeCategory
+//
+//  Created by CYT on 2017/2/21.
+//  Copyright © 2017年 CYT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WholeCategory/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  WholeCategory
+//
+//  Created by CYT on 2017/2/21.
+//  Copyright © 2017年 CYT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
